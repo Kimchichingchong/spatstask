4835b3dc9b9d0902f061fc7c69a4e8b6b326f357
gnaneshgunda <gnaneshgunda1606@gmail.com>
2025-03-22 11:41:22 +0530

    added addbook

--- script.js
+++ script.js
@@ -26,4 +26,28 @@ books[i].append(removebutton);
 
 }
 
+function addbook(event){
+  event.preventDefault();
+  var bookname = document.getElementById('bookname').value;
+  var author = document.getElementById('author').value;
+  var price= document.getElementById('price').value;
+  if(bookname=="" || author=="" || price==""){
+    alert("Please fill all the fields");
+    return;
+  }
+  var book = document.createElement('div');
+  book.className = 'card';
+  book.id = bookname;
+  book.innerHTML = "<h2 class='title'>"+bookname+"</h2><h4 class='titler'>"+author+"</h4><h4 class='titler'> Price:₹"+price+"</h4><button ><a href='#'>Read More</a></button><button><a href='#'>Add To Wishlist</a></button><button><a href='#'>Buy Now</a></button>" ;
+  const removebutton = document.createElement('button');
+  removebutton.innerHTML = 'Remove';
+  removebutton.style.color="white";
+  removebutton.onclick = function() {
+        book.remove();
+  }
+  book.append(removebutton);
+  document.getElementsByClassName('container')[0].append(book);
+}
+
+
 
